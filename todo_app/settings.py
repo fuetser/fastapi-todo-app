be452e65f465129d0565e801fc295d91b41eb0b3
Alexander Zakharchuk <mystang.pro@gmail.com>
2021-05-25 16:57:15 +0300

    Updated environment variables

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -5,12 +5,12 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     server_host: str = "localhost"
     server_port: int = 8000
-    db_url: str = os.environ.get("DATABSE_URL", "sqlite://../db.db")
-    secret_key: str = os.environ.get("SECRET_KEY", "1234567890")
+    db_url: str = os.environ.get("DATABSE_URL")
+    secret_key: str = os.environ.get("SECRET_KEY")
     reload: bool = True
     workers: int = 4
     jwt_token_expiration: int = 1800
-    jwt_algorithm: str = os.environ.get("JWT_ALGORITHM", "HS256")
+    jwt_algorithm: str = os.environ.get("JWT_ALGORITHM")
 
     class Config:
         env_file = ".env"
